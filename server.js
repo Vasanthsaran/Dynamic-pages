ff05c0dd502a670fc03b5c01cdd0e3c8f1601473
Vasanthsaran <vasanthsaran64@gmail.com>
2026-08-26 11:04:34 +0530

    Dynamic pages okay

--- server.js
+++ server.js
@@ -19,6 +19,4 @@ app.get("/Day65", (req, res) => {
     res.send("Day65- 05-08-2026 learn about networking,JSON,DOM Tested the systems setup website made for KIET bootcamp.  How the query goes from browser to server, request return modes." )
 });
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
-});
+export default app;

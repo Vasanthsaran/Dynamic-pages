24819919ea9ac73696b962e3fbf7fddda1fecc8e
Vasanthsaran <vasanthsaran64@gmail.com>
2026-08-26 13:52:57 +0530

    fixed some errors

--- server.js
+++ server.js
@@ -9,14 +9,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
 
+
 app.get("/Day65", (req, res) => {
     res.send("Day65- 05-08-2026 learn about networking,JSON,DOM Tested the systems setup website made for KIET bootcamp.  How the query goes from browser to server, request return modes." )
 });
 
-export default app;
+
+ export default app

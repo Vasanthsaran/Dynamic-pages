06a91280aee9095c2f50304eed52d4f6e30e5106
Vasanthsaran <vasanthsaran64@gmail.com>
2026-08-26 10:45:45 +0530

    image changed

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get("/",(req,res)=>{
-    res.sendFile("./public/index.html")
+    res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
 

f1aa268f1fb9bd32605d618d62f9a361a5867803
Vasanthsaran <vasanthsaran64@gmail.com>
2026-08-26 14:42:07 +0530

    added security

--- server.js
+++ server.js
@@ -9,17 +9,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.static(path.join(__dirname, "public"), { index: false }));
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname, "public", "index.html"))
+    res.sendFile(path.join(__dirname, "public", "login.html"))
 })
 
+app.post("/login",(req,res)=>{
+    username=req.body.username;
+    pass=req.body.password;
 
+    if(username=="vasanth" && pass=="vasanth@123"){
+        res.sendFile(path.join(__dirname, "public", "index.html"))
+    } else {
+        res.send("You can't access the page. OKAY!");
+        
+    }
+})
 
 app.get("/Day65", (req, res) => {
     res.send("Day65- 05-08-2026 learn about networking,JSON,DOM Tested the systems setup website made for KIET bootcamp.  How the query goes from browser to server, request return modes." )
 });
 
 
- export default app
+
+//  export default app
